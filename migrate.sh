bbd0571d67a0d192604692c501f4c090fd43aa96
Ryan Teoh <ryan@latch.bio>
2025-07-07 12:35:39 -0700

    update migrate script

--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,17 @@
-cp -TRv .jupyter/ /root/.jupyter/
+rm --recursive --force /root/.jupyter
+cp --recursive .jupyter/ /root/.jupyter/
 cat ./rserver.conf > /etc/rstudio/reserver.conf
+
+/opt/mamba/envs/jupyterlab/bin/pip install \
+    --quiet \
+    jupyterlab-code-formatter \
+    jupyter-fs \
+    jupyterlab-git
+
+echo 'restarting rstudio-server'
+systemctl --user unmask rstudio-server
+systemctl --user restart rstudio-server
+systemctl --user mask rstudio-server
+
+echo 'restarting jupyterlab'
+systemctl --user restart jupyterlab
